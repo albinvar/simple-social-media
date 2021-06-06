95440169814a55fd7082b30dc24c8b89d0b87d89
Albin Varghese <albin8285@gmail.com>
2021-06-06 23:17:29 +0530

    create like model with all

---
+++ database/factories/LikeFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Like;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class LikeFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Like::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}

fcd1f8148bd623d5b328307d8a2f054a222857fc
Albin Varghese <albin8285@gmail.com>
2021-06-06 21:55:18 +0530

    configure jetstream

--- config/jetstream.php
+++ config/jetstream.php
@@ -42,8 +42,8 @@ return [
     */
 
     'features' => [
-        // Features::termsAndPrivacyPolicy(),
-        // Features::profilePhotos(),
+        Features::termsAndPrivacyPolicy(),
+        Features::profilePhotos(),
         // Features::api(),
         // Features::teams(['invitations' => true]),
         Features::accountDeletion(),

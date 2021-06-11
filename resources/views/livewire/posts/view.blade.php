48d8a2c69e7acba5dec02427ec64b508da3a9af1
Albin Varghese <albin8285@gmail.com>
2021-06-11 15:38:06 +0530

    show no posts found on 0 posts

--- resources/views/livewire/posts/view.blade.php
+++ resources/views/livewire/posts/view.blade.php
@@ -61,7 +61,24 @@
             </div>
         </div>
         @empty
-        No Posts
+        <div class="flex flex-col my-12 md:mt-56 lg:mt-56">
+            <div class="bg-white shadow-md  rounded-3xl p-4">
+                <div class="flex-none lg:flex">
+                    <div class=" h-full w-full lg:h-48 lg:w-48   lg:mb-0 mb-3">
+                        <img src="{{ asset('images/no-posts.png') }}"
+                            alt="Just a flower" class=" w-full  object-scale-down lg:object-cover  lg:h-48 rounded-2xl">
+                    </div>
+                    <div class="flex-auto ml-3 justify-evenly py-2">
+                        <div class="flex flex-wrap ">
+                            
+                            <h2 class="flex-auto text-lg text-center font-medium">{{ __('No Posts found..!!!') }}</h2>
+                        </div>
+                        <p class="mt-3"></p>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
         @endforelse
         
         {{ $posts->links() }}

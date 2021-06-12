94938b084f067e3c057926c455210e9a4497edd6
Albin Varghese <albin8285@gmail.com>
2021-06-12 17:22:26 +0530

    create new component

---
+++ app/Http/Livewire/Posts/Manage.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Posts;
+
+use Livewire\Component;
+
+class Manage extends Component
+{
+    public function render()
+    {
+        return view('livewire.posts.manage');
+    }
+}

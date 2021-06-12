ad395a303840e9f4c84c34f7c51b4f03ad9f19e3
Albin Varghese <albin8285@gmail.com>
2021-06-12 18:46:40 +0530

    create new Edit component

---
+++ app/Http/Livewire/Posts/Edit.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Posts;
+
+use Livewire\Component;
+
+class Edit extends Component
+{
+    public function render()
+    {
+        return view('livewire.posts.edit');
+    }
+}

cf890360b9974d6b939dd5685c1bed6261787e69
Albin Varghese <albin8285@gmail.com>
2021-06-14 19:39:30 +0530

    get client ip instead of balancer ip

--- app/Http/Livewire/Posts/Create.php
+++ app/Http/Livewire/Posts/Create.php
@@ -26,15 +26,11 @@ class Create extends Component
              'body' => 'required|max:1000',
              'photo' => 'image|max:1024',
             ];
-            
-    private function getIp()
-	{
-	    return file_get_contents("http://checkip.amazonaws.com/");
-	}
     
     public function mount()
     {
-    	$ipAddress = str_replace("\n", "", $this->getIp());
+    	
+    	$ipAddress = $this->getIp();
         $position = Location::get($ipAddress);
         
         if ($position) {
@@ -80,4 +76,19 @@ class Create extends Component
     {
         return view('livewire.posts.create');
     }
+    
+    
+    private function getIp(){
+    foreach (array('HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'REMOTE_ADDR') as $key){
+        if (array_key_exists($key, $_SERVER) === true){
+            foreach (explode(',', $_SERVER[$key]) as $ip){
+                $ip = trim($ip); // just to be safe
+                if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE) !== false){
+                    return $ip;
+                }
+            }
+        }
+    }
+    return request()->ip(); // it will return server ip when no client ip found
+	}
 }

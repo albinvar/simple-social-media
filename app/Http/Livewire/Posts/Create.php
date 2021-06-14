ce1bc1bfb856b0d8563c79204156da4d308f8d5a
Albin Varghese <albin8285@gmail.com>
2021-06-14 19:40:16 +0530

    php-cs-fix

--- app/Http/Livewire/Posts/Create.php
+++ app/Http/Livewire/Posts/Create.php
@@ -29,8 +29,7 @@ class Create extends Component
     
     public function mount()
     {
-    	
-    	$ipAddress = $this->getIp();
+        $ipAddress = $this->getIp();
         $position = Location::get($ipAddress);
         
         if ($position) {
@@ -78,17 +77,18 @@ class Create extends Component
     }
     
     
-    private function getIp(){
-    foreach (array('HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'REMOTE_ADDR') as $key){
-        if (array_key_exists($key, $_SERVER) === true){
-            foreach (explode(',', $_SERVER[$key]) as $ip){
-                $ip = trim($ip); // just to be safe
-                if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE) !== false){
-                    return $ip;
+    private function getIp()
+    {
+        foreach (array('HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'REMOTE_ADDR') as $key) {
+            if (array_key_exists($key, $_SERVER) === true) {
+                foreach (explode(',', $_SERVER[$key]) as $ip) {
+                    $ip = trim($ip); // just to be safe
+                    if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE) !== false) {
+                        return $ip;
+                    }
                 }
             }
         }
+        return request()->ip(); // it will return server ip when no client ip found
     }
-    return request()->ip(); // it will return server ip when no client ip found
-	}
 }

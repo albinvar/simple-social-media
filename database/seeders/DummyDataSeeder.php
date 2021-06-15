82c593a90b79a2e4258fbacf7c81959b7b146928
Albin Varghese <albin8285@gmail.com>
2021-06-16 00:24:06 +0530

    create dummydata for testing

---
+++ database/seeders/DummyDataSeeder.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Media;
+use App\Models\Post;
+use App\Models\Like;
+use App\Models\Comment;
+use File;
+
+class DummyDataSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+       $path = storage_path('app/public/post-photos');
+
+	    if(!File::isDirectory($path)){
+
+	        File::makeDirectory($path, 0777, true, true);
+
+	    }
+	
+	    
+    
+        User::factory()->create(['email' => 'admin@gmail.com']);
+        
+	    User::factory(40)->create()->each(function ($user) {
+  
+		    Post::factory(random_int(1,10))->create(['user_id'=>$user->id])->each(function ($post) use ($user) {
+		    	Media::factory()->create(['post_id'=>$post->id]);
+				Like::factory()->create(['post_id'=>$post->id, 'user_id' => $user->id]);
+				Comment::factory(random_int(3,27))->create(['post_id'=>$post->id, 'user_id' => $user->id]);
+			});
+		});
+		
+		$this->call(LikeSeeder::class);
+    }
+}

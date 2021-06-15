c145fc24bfcc9462c696f4886d3ebbfc5adef7b6
Albin Varghese <albin8285@gmail.com>
2021-06-16 00:22:13 +0530

    add fillbale fields

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Role extends Model
 {
     use HasFactory;
+    
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'id',
+        'name',
+    ];
 }

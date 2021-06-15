68572521ae2547f37823c5030b7bbde47dad5799
Albin Varghese <albin8285@gmail.com>
2021-06-16 00:23:37 +0530

    removed all methods required for development

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,12 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use App\Models\User;
-use App\Models\Media;
-use App\Models\Post;
-use App\Models\Like;
-use App\Models\Comment;
-use File;
+use App\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -19,27 +14,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-    	$path = storage_path('app/public/post-photos');
-
-	    if(!File::isDirectory($path)){
-
-	        File::makeDirectory($path, 0777, true, true);
-
-	    }
-	
-	    
-    
-        User::factory()->create(['email' => 'admin@gmail.com']);
-        
-	    User::factory(40)->create()->each(function ($user) {
-  
-		    Post::factory(random_int(1,10))->create(['user_id'=>$user->id])->each(function ($post) use ($user) {
-		    	Media::factory()->create(['post_id'=>$post->id]);
-				Like::factory()->create(['post_id'=>$post->id, 'user_id' => $user->id]);
-				Comment::factory(random_int(3,27))->create(['post_id'=>$post->id, 'user_id' => $user->id]);
-			});
-		});
-		
-		$this->call(LikeSeeder::class);
+    	$this->call(RoleSeeder::class);
 	}
 }

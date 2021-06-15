15942c41f3201938db62885f1f0e2a792b6d9e39
Albin Varghese <albin8285@gmail.com>
2021-06-16 02:11:43 +0530

    create belongs to relationship with roles

--- app/Models/User.php
+++ app/Models/User.php
@@ -65,4 +65,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Post::class);
     }
+    
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
+    }
 }

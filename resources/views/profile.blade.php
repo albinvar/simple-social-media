dcacdec445844db1a12ac1640d9bee5d46c6b261
Albin Varghese <albin8285@gmail.com>
2021-06-17 00:22:05 +0530

    create anchor tp profile on all posts

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -8,25 +8,28 @@
 </style>
 
     <!-- component -->
-<!-- eslint-disable -->
-
-    <div class="bg-white my-12 pb-6 w-full justify-center items-center overflow-hidden md:max-w-sm rounded-lg shadow-sm mx-auto">
-      <div class="relative h-40">
-        <img class="absolute h-full w-full object-cover" src="https://images.unsplash.com/photo-1448932133140-b4045783ed9e?ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=80">
-      </div>
-      <div class="relative shadow mx-auto h-24 w-24 -my-12 border-white rounded-full overflow-hidden border-4">
-        <img class="object-cover w-full h-full" src="{{ $user->profile_photo_url }}">
-      </div>
-      <div class="mt-16">
-        <h1 class="text-lg text-center font-semibold">
-          {{ $user->name }} 
-        </h1>
-        <p class="text-sm text-gray-600 text-center">
+    	
+    <div class="bg-white my-12 pb-6 w-full justify-center items-center overflow-hidden md:max-w-sm rounded-lg shadow-sm mx-auto">
+      <div class="relative h-40">
+        <img class="absolute h-full w-full object-cover" src="https://images.unsplash.com/photo-1448932133140-b4045783ed9e?ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=80">
+      </div>
+
+      <div class="relative shadow mx-auto h-24 w-24 -my-12 border-white rounded-full overflow-hidden border-4">
+        <img class="object-cover w-full h-full" src="{{ $user->profile_photo_url }}">
+      </div>
+
+      <div class="mt-16">
+        <h1 class="text-lg text-center font-semibold">
+          {{ $user->name }} 
+        </h1>
+        <p class="text-sm text-gray-600 text-center">
           {{ '@' . $user->username }}
-        </p>
-      </div>
-      <div class="mt-6 pt-3 flex flex-wrap mx-6 border-t">
-        <divclass="py-4 flex justify-center items-center w-full divide-x divide-gray-400 divide-solid">
+        </p>
+      </div>
+
+      <div class="mt-6 pt-3 flex flex-wrap mx-6 border-t">
+        <div
+		class="py-4 flex justify-center items-center w-full divide-x divide-gray-400 divide-solid">
 			<span class="text-center px-4">
 				<span class="font-bold text-gray-700">{{ $likes }}</span>
 				<span class="text-gray-600">Likes</span>
@@ -34,15 +37,15 @@
 			<span class="text-center px-4">
 				<span class="font-bold text-gray-700">{{ $comments }}</span>
 				<span class="text-gray-600">Comments</span>
-			</span>
+			</span>
+
 			<span class="text-center px-4">
 				<span class="font-bold text-gray-700">{{ $posts }}</span>
 				<span class="text-gray-600">Posts</span>
 			</span>
-		</div>
-      </div>
+		</div>
+      </div>
     </div>
-    
 </div>
             
 </x-app-layout>

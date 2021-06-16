dd567cbfa08dfeba5d85aad067a62abc03ebdd12
Albin Varghese <albin8285@gmail.com>
2021-06-16 12:38:10 +0530

    add anchor to sign up page on login page

--- resources/views/vendor/jetstream/components/authentication-card.blade.php
+++ resources/views/vendor/jetstream/components/authentication-card.blade.php
@@ -6,4 +6,10 @@
     <div class="w-11/12 lg:w-full md:w-full sm:mt-6 sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:p-6 rounded-lg mb-6">
         {{ $slot }}
     </div>
+    
+    @if(!empty($anchor))
+    <div class="mt-6 overflow-hidden text-sm ">
+        {{ $anchor }}
+    </div>
+    @endif
 </div>

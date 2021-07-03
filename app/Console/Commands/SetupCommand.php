3509f8c860ec8d0505f8bdfda01fad38a19e1ddc
Albin Varghese <albin8285@gmail.com>
2021-07-03 17:37:54 +0530

    create custom command

---
+++ app/Console/Commands/SetupCommand.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class SetupCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'setup';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Does all necessary tasks';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+    	$this->newline();
+    	$this->comment('Setting Up Database');
+        $this->call('migrate:refresh', ['--seed' => true]);
+        
+        $this->newline();
+        $this->comment('Setting Up Storage');
+        try {
+	        $this->callSilently('storage:link');
+		} catch(\Exception $e) {
+			$this->newline();
+			$this->error('Error encountered');
+		}
+    }
+}

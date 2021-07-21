235206ab929c8c31296ba5aa7efff5a6bf075b08
Albin Varghese <albin8285@gmail.com>
2021-07-22 02:46:26 +0530

    basic fixes

--- app/Http/Livewire/Users/Edit.php
+++ app/Http/Livewire/Users/Edit.php
@@ -46,7 +46,7 @@ class Edit extends Component
             'name' => ['required', 'max:50'],
             'username' => ['string', 'max:60', Rule::unique('users')->ignore($this->user->id)],
             'email' => ['required', 'max:225', Rule::unique('users')->ignore($this->user->id)],
-            'is_private' => ['required', 'in:0,1'],
+            'is_private' => ['nullable', 'in:0,1'],
             'role_id' => ['required', 'in:1,2'],
             'password' => 'nullable|string|max:225|confirmed',
         ]);
@@ -55,7 +55,12 @@ class Edit extends Component
         $this->user->email = $this->email;
         $this->user->username = $this->username;
         $this->user->role_id = $this->role_id;
-        $this->user->is_private = $this->is_private;
+       
+       if(!is_null($this->is_private)){
+        	$this->user->is_private = $this->is_private;
+        }
+       
+        
         if(!empty($this->password)){
         	$this->user->password = Hash::make($this->password);
         }

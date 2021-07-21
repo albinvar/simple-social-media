d15fe366e361d8be883639b6bcaa9d3a7a8aee90
Albin Varghese <albin8285@gmail.com>
2021-07-21 19:50:11 +0530

    show users details in table

--- app/Http/Livewire/Users/Index.php
+++ app/Http/Livewire/Users/Index.php
@@ -3,9 +3,18 @@
 namespace App\Http\Livewire\Users;
 
 use Livewire\Component;
+use App\Models\User;
 
 class Index extends Component
 {
+	
+	public $users;
+	
+	public function mount()
+	{
+		$this->users = User::all();
+	}
+	
     public function render()
     {
         return view('livewire.users.index');
--- resources/views/livewire/users/index.blade.php
+++ resources/views/livewire/users/index.blade.php
@@ -1,8 +1,8 @@
 <div>
-<div class="flex flex-col m-2">
-  <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+<div class="flex flex-col m-2 ">
+  <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8 ">
     <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-      <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+      <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg rounded-xl shadow-sm">
         <table class="min-w-full divide-y divide-gray-200">
           <thead class="bg-gray-50">
             <tr>
@@ -10,7 +10,7 @@
                 Name
               </th>
               <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Title
+                Username
               </th>
               <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                 Status
@@ -24,25 +24,27 @@
             </tr>
           </thead>
           <tbody class="bg-white divide-y divide-gray-200">
+          @foreach($users as $user)
             <tr>
               <td class="px-6 py-4 whitespace-nowrap">
                 <div class="flex items-center">
                   <div class="flex-shrink-0 h-10 w-10">
-                    <img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60" alt="">
+                  
+                    <img class="h-10 w-10 rounded-full" src="{{ $user->profile_photo_url }}" alt="">
                   </div>
                   <div class="ml-4">
                     <div class="text-sm font-medium text-gray-900">
-                      Jane Cooper
+                      {{ $user->name }}
                     </div>
                     <div class="text-sm text-gray-500">
-                      jane.cooper@example.com
+                      {{ $user->email }}
                     </div>
                   </div>
                 </div>
               </td>
               <td class="px-6 py-4 whitespace-nowrap">
-                <div class="text-sm text-gray-900">Regional Paradigm Technician</div>
-                <div class="text-sm text-gray-500">Optimization</div>
+                <div class="text-sm text-gray-900">{{ $user->username }}</div>
+                <div class="text-sm text-gray-500">@if($user->is_private) Private @else Public @endif</div>
               </td>
               <td class="px-6 py-4 whitespace-nowrap">
                 <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
@@ -50,12 +52,17 @@
                 </span>
               </td>
               <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                Admin
+                @if($user->role_id === 2)
+	                Admin
+                @else
+	                User
+                @endif
               </td>
               <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
                 <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
               </td>
             </tr>
+            @endforeach
 
             <!-- More people... -->
           </tbody>

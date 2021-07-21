edd9c5ab0326ef5c64f9a8f9974dae5593d3710c
Albin Varghese <albin8285@gmail.com>
2021-07-21 17:33:02 +0530

    create User controller

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('users.index');
+    }
+
+}
